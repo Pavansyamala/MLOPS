083898769a1f41575e11b20fdf339a7ed1dcee54
Pavan Syamal <pavansyamaladata2025@gmail.com>
2024-02-11 10:20:06 +0530

    model trainer

--- src/components/modeltrainer.py
+++ src/components/modeltrainer.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import sys
+import os 
 import numpy as np 
 from src.logger.logging import logging 
 from src.exception.exception import CustomException
@@ -7,26 +8,58 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from pathlib import Path 
 
+from src.components.dataingestion import DataIngestion
+from src.components.datatransformation import DataTransformation
 from src.utils.utils import save_object , evaluate_model
-from sklearn.linear_model import LinearRegressio , Ridge , Lasso 
+from sklearn.linear_model import LinearRegression , Ridge , Lasso 
 
 @dataclass
 class ModelTrainerConfig:
     def __init__(self):
-        pass 
+        self.model_path = os.path.join("artifacts" , "model.pkl")
 
 class ModelTrainer:
     def __init__(self):
-        pass 
+        self.model_config = ModelTrainerConfig()
     
-    def initiate_model_trainer(self):
+    def initiate_model_trainer(self , train_array , test_array):
         try :
-            a = 1/0
-            pass 
+            logging.info("Model Training Started ") 
+            x_train , y_train = train_array[:,:-1] , train_array[:,-1]
+            x_test , y_test = test_array[:,:-1] , test_array[:,-1]
+
+            models = {
+                'LinearRegression' : LinearRegression(),
+                'Lasso' : Lasso(),
+                'Ridge' : Ridge()
+            }
+
+            report  = evaluate_model(x_train,y_train,x_test,y_test , models) 
+
+            logging.info("Model Training Completed")
+
+            best_score = -1*10**9 
+            best_model = 'LinearRegression'
+            for key , val in report.items():
+                logging.info(f"Model {key} is having r2_score as {val}")
+                if val > best_score :
+                    best_score = val 
+                    best_model = key 
+            
+            logging.info(f"Selected Best Model Found with the Score {best_score} and the model name is {best_model}") 
+
+            save_object(self.model_config.model_path , models[best_model])
+
+            logging.info("Succesfully Saved Best Model")
+
         except Exception as e :
-            logging.info()
+            logging.info(e)
             raise CustomException(e , sys)
 
 if __name__ == "__main__" :
-    obj = ModelTrainer()
-    obj.initiate_model_trainer()
+    obj = DataIngestion()
+    train_path , test_path = obj.initiate_data_ingestion()
+    obj1 = DataTransformation()
+    train_arr , test_arr = obj1.initiate_data_transformation(train_path , test_path) 
+    obj3 = ModelTrainer()
+    obj3.initiate_model_trainer(train_arr,test_arr)
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -23,21 +23,16 @@ def save_object(file_path, obj):
 def evaluate_model(X_train,y_train,X_test,y_test,models):
     try:
         report = {}
-        for i in range(len(models)):
-            model = list(models.values())[i]
+        for name , model in models.items():
             # Train model
             model.fit(X_train,y_train)
-
-            
-
             # Predict Testing data
             y_test_pred =model.predict(X_test)
-
             # Get R2 scores for train and test data
             #train_model_score = r2_score(ytrain,y_train_pred)
             test_model_score = r2_score(y_test,y_test_pred)
 
-            report[list(models.keys())[i]] =  test_model_score
+            report[name] =  test_model_score
 
         return report
 
