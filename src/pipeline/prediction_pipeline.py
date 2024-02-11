7e31577643cefca92e6a98ed7d67dfe09c23c3d6
Syamala Pavan <pavansyamaladata2025@gmail.com>
2024-02-11 20:32:06 +0530

    app

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -25,4 +25,30 @@ class PredictPipeline:
         
         except Exception as e :
             logging.info(e)
-            raise CustomException(e , sys)
+            raise CustomException(e , sys) 
+
+class CustomData:
+    def __init__(self , carat , depth , table , x,y,z,cut,color,clarity):
+        self.carat = carat
+        self.depth = depth
+        self.table = table
+        self.x = x
+        self.y = y 
+        self.z = z
+        self.cut = cut
+        self.color = color 
+        self.clarity = clarity
+    
+    def get_data_as_dataframe(self):
+        
+        return pd.DataFrame({
+            'carat' : self.carat , 
+            'depth' : self.depth , 
+            'table' : self.table ,
+            'x' : self.x ,
+            'y' : self.y , 
+            'z' : self.z , 
+            'cut' : self.cut , 
+            'color' : self.color , 
+            'clarity' : self.clarity
+        } , index = [0])
