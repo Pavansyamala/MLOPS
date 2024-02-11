ca3cc42a3d799fe4537b342251ae42942e30f698
Pavan Syamala <pavansyamaladata2025@gmail.com>
2024-02-11 07:34:01 +0530

    data ingestion

--- src/components/dataingestion.py
+++ src/components/dataingestion.py
@@ -6,23 +6,54 @@ from src.exception.exception import CustomException
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from pathlib import Path 
+import os 
+import requests
+from io import StringIO
 
 @dataclass
 class DataIngestionConfig:
     def __init__(self):
-        pass 
+        self.train_path = os.path.join("artifacts" , "train.csv")
+        self.test_path = os.path.join("artifacts","test.csv")
+        self.raw_path = os.path.join("artifacts" , "raw.csv")
 
 class DataIngestion:
     def __init__(self):
-        pass 
+        self.ingestion_config = DataIngestionConfig() 
     
     def initiate_data_ingestion(self):
+        logging.info("Data Ingestion Started")
         try :
-            pass 
+            url = "https://raw.githubusercontent.com/Pavansyamala/Deep-Learning/main/archive/cubic_zirconia.csv"
+            response = requests.get(url)
+            data = StringIO(response.text)
+            logging.info("Reading Data Started")
+            data = pd.read_csv(data)
+            logging.info("Reading Data Completed")
+            path_name = os.path.join(os.getcwd(),os.path.dirname(self.ingestion_config.raw_path))
+            os.makedirs(path_name , exist_ok = True) 
+            logging.info("Saving Raw Data  Started")
+            data.to_csv(self.ingestion_config.raw_path)
+            logging.info("Succesfully Saved Raw Data") 
+
+            train_data , test_data = train_test_split(data , test_size = 0.2)
+
+            logging.info("Started Saving Train and Test Data")
+            train_data.to_csv(self.ingestion_config.train_path)
+            test_data.to_csv(self.ingestion_config.test_path)
+            logging.info("Succesfully Saved Training and Testing Data")
+
+            logging.info("Data ingestion Completed")
+
+            return self.ingestion_config.train_path , self.ingestion_config.test_path
+
+
         except Exception as e :
             logging.info(e)
             raise CustomException(e , sys)
 
 if __name__ == "__main__" :
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    a,b = obj.initiate_data_ingestion()
+    print(a)
+    print(b)
